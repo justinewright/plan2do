4a57c83ee5e2dff152c1d12a61c7c35a94734e84
justine wright <wright.justine21@gmail.com>
2021-06-13 17:37:44 +0200

    feat: adds data models

---
+++ ExecuteList/ExecuteList/Models/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  ExecuteList
+//
+//  Created by Justine Wright on 2021/06/13.
+//
+
+import Foundation
+
+struct Category {
+    let name: String = ""
+    let items: Array <Item> 
+}
---
+++ ExecuteList/ExecuteList/Models/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  ExecuteList
+//
+//  Created by Justine Wright on 2021/06/13.
+//
+
+import Foundation
+
+struct Item {
+    let name: String = ""
+    let done: Bool = false
+}
