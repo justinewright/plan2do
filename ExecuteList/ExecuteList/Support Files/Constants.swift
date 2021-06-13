4813f204793b64e790de66352c0805e2d295c327
justine wright <wright.justine21@gmail.com>
2021-06-13 17:32:15 +0200

    feat: add basic ui

---
+++ ExecuteList/ExecuteList/Support Files/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ExecuteList
+//
+//  Created by Justine Wright on 2021/06/13.
+//
+
+import Foundation
+
+struct K {
+    let todoListSegueID = "todoListItems"
+    let cellID = "Cell"
+    
+}
