f559dd09c0a062b149df6c4367634bf3c381366c
justine wright <wright.justine21@gmail.com>
2021-06-13 17:42:12 +0200

    refactor: deleted and moved files

--- ExecuteList/ExecuteList/Controllers/CategoryViewController.swift
+++ ExecuteList/ExecuteList/Controllers/CategoryViewController.swift
@@ -8,22 +8,18 @@
 import UIKit
 
 class CategoryViewController: UITableViewController {
-
+    
+    var categories: Array<Category>?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return categories?.count ?? 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
